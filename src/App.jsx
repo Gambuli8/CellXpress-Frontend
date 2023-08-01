5264630395077d614395cfa4d7870f9dc361b098
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-07-31 19:39:22 -0500

    creacion card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Home from './Views/Home/Home'
 
 function App() {
   return (
     <div>
       <h1>CellXpress</h1>
+      <Home/>
     </div>
   )
 }
--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,5 +1,56 @@
-export default function Home() {
+import React from 'react'
+import Cards_Phone from '../../Components/Cards_Phone/Cards_Phone'
+export const Product = [
+  {
+    id: 1,
+    name: "Sansung Galaxy s23",
+    description: "El celular mas potente del Mundo",
+    image: "https://smselectronic.com/wp-content/uploads/2023/05/8806094711417_a.jpg"
+  },
+  {
+    id: 2,
+    name: "Sansung Galaxy s23",
+    description: "El celular mas potente del Mundo",
+    image: "https://smselectronic.com/wp-content/uploads/2023/05/8806094711417_a.jpg"
+  },
+  {
+    id: 3,
+    name: "Sansung Galaxy s23",
+    description: "El celular mas potente del Mundo",
+    image: "https://smselectronic.com/wp-content/uploads/2023/05/8806094711417_a.jpg"
+  },
+  {
+    id: 4,
+    name: "Sansung Galaxy s23",
+    description: "El celular mas potente del Mundo",
+    image: "https://smselectronic.com/wp-content/uploads/2023/05/8806094711417_a.jpg"
+  },
+  {
+    id: 5,
+    name: "Sansung Galaxy s23",
+    description: "El celular mas potente del Mundo",
+    image: "https://smselectronic.com/wp-content/uploads/2023/05/8806094711417_a.jpg"
+  },
+  {
+    id: 6,
+    name: "Sansung Galaxy s23",
+    description: "El celular mas potente del Mundo",
+    image: "https://smselectronic.com/wp-content/uploads/2023/05/8806094711417_a.jpg"
+  },
+  {
+    id: 7,
+    name: "Sansung Galaxy s23",
+    description: "El celular mas potente del Mundo",
+    image: "https://smselectronic.com/wp-content/uploads/2023/05/8806094711417_a.jpg"
+  }
+]
+
+const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Cards_Phone Product = {Product}/>
+    </div>
   )
 }
+
+export default Home
