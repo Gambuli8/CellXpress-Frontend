4e513873804379e7c791470a531d90b5c4543177
Matias Sebastian Rueda <elkema89@gmail.com>
2023-08-01 10:53:09 -0300

    primer commit clerk

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./App.css"
+import Login from "./Components/Login/Login"
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter,Routes, Route } from "react-router-dom"
+import dotenv from "dotenv"
+dotenv.config()
+
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <ClerkProviderWithRoutes />
-    </BrowserRouter>
+    <ClerkProvider>
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    </ClerkProvider>
+
   );
 }
 
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import "./App.css";
-import { ClerkProvider, SignIn } from "@clerk/clerk-react";
+import { SignIn } from "@clerk/clerk-react";
 
-if (!process.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
-const clerkPubKey = process.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 
 function Login() {
-  return (
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <SignIn />
-    </ClerkProvider>
-  );
+  return <SignIn />
 }
 
-export default App;
+export default Login;
