75055f50a73113116565b11252220e8f45dc70b3
GeroGambuli <gerogambuli2002@gmail.com>
2023-08-14 10:23:54 -0300

    .

--- src/Components/FormCarrito/formCart.jsx
+++ src/Components/FormCarrito/formCart.jsx
@@ -6,7 +6,6 @@ export default function FormCart() {
         <div className={style.form}>
             <div className={style.formHeader}>
                 <h1>Formulario de compra</h1>
-                <input type="text" placeholder='' />
             </div>
         </div>     
     </div>
