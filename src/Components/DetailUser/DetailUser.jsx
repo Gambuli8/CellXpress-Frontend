d5d1f217009a4bbcf5eaa00ef9f374639ca13a0b
Elkema89 <elkema89gmail.com>
2023-08-21 11:29:41 -0300

    edituser

--- src/Components/DetailUser/DetailUser.jsx
+++ src/Components/DetailUser/DetailUser.jsx
@@ -1,9 +1,15 @@
 import style from "./DetailUser.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useAuth } from "../../context/authContext";
-import { useParams } from "react-router-dom";
+import StarRating from "../StarRating/StarRating"
+
+import { Link, useNavigate, useParams, Navigate } from "react-router-dom";
+
+
+
 import { useState, useEffect } from "react";
 import { getOrderById, editPutUser, getUserById } from "../../Redux/Actions";
+import TextArea from "rc-textarea";
 
 const DetailUser = () => {
   const { id } = useParams();
@@ -17,6 +23,20 @@ const DetailUser = () => {
     phone: "",
     id: id,
   });
+  //perfectoooooooooooooooo
+
+  window.addEventListener('popstate', function (e) {
+    //window.location.assign("http://localhost:5173/home");
+     window.location.assign("https://pf-cell-xpress-frontend.vercel.app/home")
+  });
+
+
+  window.addEventListener('popstate', function (e) {
+    window.location.assign("http://localhost:5173/home");
+    window.location.assign("https://pf-cell-xpress-frontend.vercel.app/home")
+  });
+
+
 
   useEffect(() => {
     dispatch(getOrderById(id));
@@ -34,15 +54,17 @@ const DetailUser = () => {
       [event.target.name]: event.target.value,
     });
   };
-
+  
   const handleSubmit = () => {
-    dispatch(editPutUser(input));
-  };
 
-  console.log(input);
+   dispatch(editPutUser(input));
+     
+  };
   return (
-    <div>
-      <div className={style.containerProbando}>
+    <div className={style.container}>
+      <a className={style.btn_back} href="/home">
+              Atras
+            </a>
         <div className={style.containerEditUser}>
           <div>
             <img
@@ -52,52 +74,54 @@ const DetailUser = () => {
                   : "https://cdn-icons-png.flaticon.com/128/2986/2986624.png"
               }
               alt=""
+              className={style.img}
             />
+
           </div>
           <form onSubmit={handleSubmit} className={style.formEditUser}>
             <label>Su usuario</label>
             <input
               type="text"
               name="name"
+              className={style.input}
               placeholder={allUsuariosByID.name}
               onChange={handleChange}
-              required
             />
             <label>Su teléfono</label>
             <input
               type="number"
               name="phone"
+              className={style.input}
               placeholder={
                 allUsuariosByID.phone ? allUsuariosByID.phone : "ej: 3519887123"
               }
               onChange={handleChange}
             />
-            <button className={style.btn}>Editar usuario</button>
+            <button className={style.btn_back}>Editar usuario</button>
           </form>
         </div>
-        <div className={style.containerOrdenesCompras}>
           <h2>Ordenes de compras</h2>
+        <div className={style.containerOrdenesCompras}>
           {allOrderByID.map((elemento) => {
             return (
               <div key={elemento._id} className={style.ordenCompra}>
-                <label className={style.label}>{elemento._id}</label>
-                <label className={style.label}>{elemento.total}</label>
                 {console.log(elemento)}
                 <div className={style.ordenDetail}>
                   {elemento.products.map((e) => {
                     return (
-                      <div key={e._id}>
+                      <ul key={e._id} className={style.containerProduct}>
                         <img
                           src={e.product.image}
                           alt={e.product.title}
-                          width="150px"
-                          height="180px"
-                        />
-                        <label>{e.product.title}</label>
-                        <label>{e.product.brand}</label>
-                        <label className={style.label}>{elemento.total}</label>
-                        <label className={style.quantity}>{e.quantity}</label>
-                      </div>
+                          width="80px"
+                          height="80px"
+                          />
+                        <li className={style.label}>{e.product.title}</li>
+                        <li className={style.label}>{e.product.brand}</li>
+                        <li className={style.label}>${elemento.total}</li>
+                        <StarRating/>
+                        <li className={style.label}>{e.quantity}</li>
+                      </ul>
                     );
                   })}
                 </div>
@@ -105,7 +129,6 @@ const DetailUser = () => {
             );
           })}
         </div>
-      </div>
     </div>
   );
 };
